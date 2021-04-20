5734c7195e6e91de36e1dcc94d1ce329d62c6053
Logan <MondayHopscotch@users.noreply.github.com>
2021-04-19 18:33:58 -0600

    Initial commit

---
+++ source/entities/Player.hx
@@ -0,0 +1,29 @@
+package entities;
+
+import input.InputCalcuator;
+import input.BasicControls;
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class Player extends FlxSprite {
+	var speed:Float = 30;
+	var controls:BasicControls;
+
+	public function new() {
+		super();
+		makeGraphic(20, 20, FlxColor.BLUE);
+
+		controls = new BasicControls();
+	}
+
+	override public function update(delta:Float) {
+		super.update(delta);
+
+		var inputDir = InputCalcuator.getInputCardinal(controls);
+		if (inputDir != NONE) {
+			inputDir.asVector(velocity).scale(speed);
+		} else {
+			velocity.set();
+		}
+	}
+}
