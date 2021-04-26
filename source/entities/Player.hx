d8c5f203fcfb18a9e11e89c16de9306cc9aa2ed1
Logan <monday.hopscotch@gmail.com>
2021-04-25 22:59:26 -0600

    don't render extra tail while moving in/out

--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -376,7 +376,7 @@ class Player extends Moleness {
 
 	private function updateTail(delta:Float) {
 		if (animation.name != null) {
-			if (animation.name.contains("turn") || animation.name.contains(FALLING)) {
+			if (animation.name.contains("turn") || animation.name.contains(FALLING) || isTransitioningBetweenLayers) {
 				tail.visible = false;
 				return;
 			}
